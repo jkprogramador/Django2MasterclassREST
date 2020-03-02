58b8b8127c26c6f441f5b5f3c3488f3c512372a4
Jack Lee <jack.lee.1981@pm.me>
2020-03-02 16:52:13 -0300

    Complete REST lesson.

---
+++ mysite/movies/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class MovieData(models.Model):
+
+  name = models.CharField(max_length = 200)
+  duration = models.FloatField()
+  rating = models.FloatField()
+  genre = models.CharField(max_length = 200, default = 'action')
+  image = models.ImageField(upload_to = 'images/', default = 'images/none/noimg.jpg')
+
+  def __str__(self):
+    return self.name
